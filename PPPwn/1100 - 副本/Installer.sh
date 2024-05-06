528c44995ddcc4457a997e0be6b4fddd9a4edbd6
newpsgo <yanchao1015@gmail.com>
2024-05-06 19:17:09 +0800

    .

--- PPPwn/1100 - 副本/Installer.sh
+++ PPPwn/1100 - 副本/Installer.sh
@@ -1,20 +1,20 @@
-apt-get update 
-apt-get upgrade 
+sudo apt-get update 
+sudo apt-get upgrade 
 
-apt install python3 
+sudo apt install python3 
 python3 --version
 
-apt install python3-pip 
+sudo apt install python3-pip 
 pip3 --version
 
-apt install git
+sudo apt install git
 git —version
 
-apt install gcc
+sudo apt install gcc
 gcc --version
 
 git clone --recursive https://github.com/TheOfficialFloW/PPPwn
 cd PPPwn
-pip install -r requirements.txt ﻿--break-system-packages
+sudo pip install -r requirements.txt ﻿--break-system-packages
 make -C stage1 FW=1100 clean && make -C stage1 FW=1100
 make -C stage2 FW=1100 clean && make -C stage2 FW=1100
--- PPPwn/1100/go.sh
+++ PPPwn/1100/go.sh
@@ -1,1 +1,1 @@
-sudo python3 pppwn.py --interface=enp0s1 --fw=1100
+python3 pppwn.py --interface=enp0s1 --fw=1100
